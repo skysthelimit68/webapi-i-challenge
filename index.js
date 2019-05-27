5c31e2b7e87c494ed2b3264eb1e368fb8aea976a
Ming Liu <mliu@MacbookPro15.local>
2019-05-27 19:38:37 -0400

    put, post, get, get by id, delete

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const data = require("./data/db.js");
 
 // creates an express application using the express module
 const server = express();
+server.use(express.json());
 
 const hobbits = [
   {
@@ -29,7 +30,7 @@ server.get("/hobbits", (req, res) => {
   res.status(200).json(hobbits);
 });
 
-server.get("/users", (req, res) => {
+server.get("/api/users", (req, res) => {
   data
     .find()
     .then(response => {
@@ -37,9 +38,96 @@ server.get("/users", (req, res) => {
       res.status(200).json(response);
     })
     .catch(err => {
-      res.status(500).send("something happened");
+      res
+        .status(500)
+        .json({ error: "The users information could not be retrieved." });
     });
 });
+
+server.post("/api/users", (req, res) => {
+  console.log(req);
+  const username = req.body.name;
+  const userbio = req.body.bio;
+  if (!username || !userbio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+    return;
+  }
+
+  data
+    .insert({ name: username, bio: userbio })
+    .then(response => {
+      console.log(response);
+      res.status(201).json(response);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "There was an error while saving the user to the database"
+      });
+    });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  data
+    .findById(req.params.id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The user information could not be retrieved." });
+    });
+});
+
+server.put("/api/users/:id", (req, res) => {
+  const { name, bio } = req.body;
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  }
+  data
+    .update(req.params.id, { name, bio })
+    .then(newUser => {
+      if (newUser === 0) {
+        res.status(404).json({
+          message: "The user with the specified ID does not exist."
+        });
+      } else {
+        res.status(200).json(newUser);
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The user information could not be modified." });
+    });
+});
+
+server.delete("/api/users/:id", (req, res) => {
+  data
+  .remove(req.params.id)
+  .then ( user => {
+    if (user === 0) {
+      res.status(404).json({ message: "The user with the specified ID does not exist." })
+    } else {
+      res.status(200).json({ message: "The user has been deleted."});
+    }
+  })
+  .catch (err => {
+    res.status(500).json({ error: "The user could not be removed" })
+  })
+
+})
+
 
 // once the server is fully configured we can have it "listen" for connections on a particular "port"
 // the callback function passed as the second argument will run once when the server starts
